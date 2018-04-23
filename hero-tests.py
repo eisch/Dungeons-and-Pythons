25dbd1f2d9227cfcfd960cacab2e3b8faa001183
Elitsa Shishkova <elitsa.shishkova5@gmail.com>
2018-04-23 20:15:15 +0300

    Add Hero init tests

---
+++ hero-tests.py
@@ -0,0 +1,43 @@
+import unittest
+from hero import Hero
+
+
+class HeroTests(unittest.TestCase):
+    def test_init_create_hero(self):
+        Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=5)
+
+    def test_assert_type_name(self):
+        with self.assertRaises(AssertionError):
+            Hero(name=5, title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=5)
+
+    def test_assert_type_title(self):
+        with self.assertRaises(AssertionError):
+            Hero(name="Bron", title=5, health=100, mana=100, mana_regeneration_rate=5)
+
+    def test_assert_type_health(self):
+        with self.assertRaises(AssertionError):
+            Hero(name="Bron", title="Dragonslayer", health=100.1, mana=100, mana_regeneration_rate=5)
+
+    def test_assert_type_mana(self):
+        with self.assertRaises(AssertionError):
+            Hero(name="Bron", title="Dragonslayer", health=100, mana=10.4, mana_regeneration_rate=5)
+
+    def test_assert_type_mana_regeneration_rate(self):
+        with self.assertRaises(AssertionError):
+            Hero(name="Bron", title="Dragonslayer", health=100, mana=10.4, mana_regeneration_rate=5.1)
+
+    def test_check_health_negative(self):
+        with self.assertRaises(ValueError):
+            Hero(name="Bron", title="Dragonslayer", health=-100, mana=100, mana_regeneration_rate=5)
+
+    def test_check_mana_negative(self):
+        with self.assertRaises(ValueError):
+            Hero(name="Bron", title="Dragonslayer", health=100, mana=-100, mana_regeneration_rate=5)
+
+    def test_check_mana_regeneration_rate_negative(self):
+        with self.assertRaises(ValueError):
+            Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=-5)
+
+
+if __name__ == "__main__" :
+    unittest.main()
