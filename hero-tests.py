157ba66b06dbd9165166e1c0843a333ec7a99a4e
Elitsa Shishkova <elitsa.shishkova5@gmail.com>
2018-04-23 20:39:49 +0300

    Add getters and setters for health and mana + tests

--- hero-tests.py
+++ hero-tests.py
@@ -19,19 +19,27 @@ class HeroTests(unittest.TestCase):
 
     def test_assert_type_health(self):
         with self.assertRaises(AssertionError):
-            Hero(name="Bron", title="Dragonslayer", health=100.1, mana=100, mana_regeneration_rate=5)
+            Hero(name="Bron", title="Dragonslayer", health=100.1, mana=[100], mana_regeneration_rate=5)
 
     def test_assert_type_mana(self):
         with self.assertRaises(AssertionError):
-            Hero(name="Bron", title="Dragonslayer", health=100, mana=10.4, mana_regeneration_rate=5)
+            Hero(name="Bron", title="Dragonslayer", health=100, mana=[10.4], mana_regeneration_rate=5)
 
     def test_assert_type_mana_regeneration_rate(self):
         with self.assertRaises(AssertionError):
-            Hero(name="Bron", title="Dragonslayer", health=100, mana=10.4, mana_regeneration_rate=5.1)
+            Hero(name="Bron", title="Dragonslayer", health=100, mana=[10.4], mana_regeneration_rate=5.1)
 
     def test_check_health_negative(self):
         with self.assertRaises(ValueError):
             Hero(name="Bron", title="Dragonslayer", health=-100, mana=100, mana_regeneration_rate=5)
+
+    def test_check_health_more_than_100(self):
+        with self.assertRaises(ValueError):
+            Hero(name="Bron", title="Dragonslayer", health=102, mana=100, mana_regeneration_rate=5)
+
+    def test_check_mana_more_than_100(self):
+        with self.assertRaises(ValueError):
+            Hero(name="Bron", title="Dragonslayer", health=100, mana=102, mana_regeneration_rate=5)
 
     def test_check_mana_negative(self):
         with self.assertRaises(ValueError):
@@ -44,6 +52,18 @@ class HeroTests(unittest.TestCase):
     def test_known_as(self):
         expected = "Bron the Dragonslayer"
         self.assertEqual(self.test_hero.known_as(), expected)
+
+    def test_is_alive(self):
+        self.assertTrue(self.test_hero.is_alive() is True)
+
+    def test_can_cast(self):
+        self.assertTrue(self.test_hero.can_cast() is True)
+
+    def test_get_health(self):
+        self.assertEqual(self.test_hero.get_health(), 100)
+
+    def test_get_mana(self):
+        self.assertEqual(self.test_hero.get_mana(), 100)
 
 
 if __name__ == "__main__":
--- hero.py
+++ hero.py
@@ -4,10 +4,12 @@ class Hero:
                  mana=100, mana_regeneration_rate=2):
         assert type(name) is str
         assert type(title) is str
-        assert type(health) is int
-        assert type(mana) is int
-        assert type(mana_regeneration_rate) is int
+        assert type(health) is float or type(health) is int
+        assert type(mana) is float or type(mana) is int
+        assert type(mana_regeneration_rate) is float or type(mana_regeneration_rate) is int
         if health < 0 or mana < 0 or mana_regeneration_rate < 0:
+            raise ValueError
+        if health > 100 or mana > 100:
             raise ValueError
         self.name = name
         self.title = title
@@ -17,3 +19,28 @@ class Hero:
 
     def known_as(self):
         return f"{self.name} the {self.title}"
+
+    def is_alive(self):
+        return self.health > 0
+
+    def can_cast(self):
+        return self.mana > 0
+
+    def get_health(self):
+        return self.health
+
+    def get_mana(self):
+        return self.mana
+
+    def take_healing():
+        pass
+
+    def take_mana():
+        pass
+
+    def attack():
+        pass
+
+    def take_damage(damage):
+        pass
+
