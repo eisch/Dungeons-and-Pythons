45b894a55f4293a8ca454aeb69762d10cb83ebab
KristinStefanova <KristinStefanova96@gmail.com>
2018-04-23 19:55:58 +0300

    add Enemies test class

--- enemy.py
+++ enemy.py
@@ -1,2 +1,29 @@
 class Enemy:
-    
+    def __init__(self, health=100, mana=100, damage=20):
+        self.health = health
+        self.mana = mana
+        self.damage = damage
+
+    def is_alive():
+        pass
+
+    def defcan_cast():
+        pass
+
+    def get_health():
+        pass
+
+    def get_mana():
+        pass
+
+    def take_healing():
+        pass
+
+    def take_mana():
+        pass
+
+    def attack():
+        pass
+
+    def take_damage(damage):
+        pass
