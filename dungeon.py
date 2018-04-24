80d4a5991f716544f2a6c2bec64891ad0a28c5a0
Elitsa Shishkova <elitsa.shishkova5@gmail.com>
2018-04-24 17:58:11 +0300

    Add print method

--- dungeon.py
+++ dungeon.py
@@ -2,3 +2,14 @@ class Dungeon:
     def __init__(self, file_name):
         assert type(file_name) is str
         self.file_name = file_name
+
+    def read_from_file(self):
+        result = []
+        with open(self.file_name) as f:
+            for line in f.readlines():
+                result.append([letter for letter in line][:len(line) - 1])
+        return result
+
+    def print_map(self):
+        for line in self.read_from_file():
+            print("".join(line))
--- dungeon_tests.py
+++ dungeon_tests.py
@@ -3,9 +3,15 @@ from dungeon import Dungeon
 
 
 class DungeonTests(unittest.TestCase):
+    def setUp(self):
+        self.d = Dungeon('level1.txt')
+
     def test_init_name_type(self):
         with self.assertRaises(AssertionError):
             Dungeon(5)
+
+    def test_print(self):
+        self.d.print_map()
 
 
 if __name__ == "__main__":
