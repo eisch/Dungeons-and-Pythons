bc2b8a2b195ad3cd06a8a7422106a2b07823708b
Elitsa Shishkova <elitsa.shishkova5@gmail.com>
2018-04-25 01:41:23 +0300

    Add spawn method and tests

--- dungeon.py
+++ dungeon.py
@@ -1,9 +1,13 @@
+from hero import Hero
+
+
 class Dungeon:
     def __init__(self, file_name):
         assert type(file_name) is str
         self.file_name = file_name
+        self.map = self.read_from_file_map()
 
-    def read_from_file(self):
+    def read_from_file_map(self):
         result = []
         with open(self.file_name) as f:
             for line in f.readlines():
@@ -11,5 +15,18 @@ class Dungeon:
         return result
 
     def print_map(self):
-        for line in self.read_from_file():
+        for line in self.map:
             print("".join(line))
+
+    def spawn(self, hero):
+        assert type(hero) is Hero
+        for ind, element in enumerate(self.map):
+            if 'S' in element:
+                index = element.index('S')
+                element[index] = "H"
+                return True
+        return False
+
+    def move(self, direction):
+        assert type(direction) is str
+
--- dungeon_tests.py
+++ dungeon_tests.py
@@ -1,17 +1,31 @@
 import unittest
 from dungeon import Dungeon
+from hero import Hero
 
 
 class DungeonTests(unittest.TestCase):
     def setUp(self):
         self.d = Dungeon('level1.txt')
+        self.test_hero = Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=5)
 
     def test_init_name_type(self):
         with self.assertRaises(AssertionError):
             Dungeon(5)
 
-    def test_print(self):
-        self.d.print_map()
+    def test_spawn_type(self):
+        with self.assertRaises(AssertionError):
+            self.d.spawn(5)
+
+    def test_spawn_first_occurence(self):
+        self.assertTrue(self.d.spawn(self.test_hero))
+
+    def test_spawn_no_spawns(self):
+        self.d.spawn(self.test_hero)
+        self.assertFalse(self.d.spawn(self.test_hero))
+
+    def test_move_direction_type(self):
+        with self.assertRaises(AssertionError):
+            self.d.move(5)
 
 
 if __name__ == "__main__":
