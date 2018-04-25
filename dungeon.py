29b27cbd5f0d0d7ffbd7f64cac53966f00c49b59
Elitsa Shishkova <elitsa.shishkova5@gmail.com>
2018-04-25 15:26:51 +0300

    Add part of move method and tests

--- dungeon.py
+++ dungeon.py
@@ -27,6 +27,72 @@ class Dungeon:
                 return True
         return False
 
+    def is_valid_coordinate(self, x, y):
+        return x in range(0, len(self.map)) and y in range(0, len(self.map[0]))
+
+    def get_coordinate(self, element):
+        for index, list_elem in enumerate(self.map):
+            if element in list_elem:
+                ind = list_elem.index(element)
+                return (index, ind)
+        return False
+
+    def hero_coordinates(self):
+        return self.get_coordinate('H')
+
+    def get_element_from_coordinates(self, x, y):
+        if self.is_valid_coordinate(x, y):
+            return self.map[x][y]
+        else:
+            return None
+
+    def check_valid_move(self, direction):
+        hero_x, hero_y = self.hero_coordinates()
+        if direction == "right":
+            return self.is_valid_coordinate(hero_x, hero_y + 1)
+        elif direction == "left":
+            return self.is_valid_coordinate(hero_x, hero_y - 1)
+        elif direction == "up":
+            return self.is_valid_coordinate(hero_x - 1, hero_y)
+        elif direction == "down":
+            return self.is_valid_coordinate(hero_x + 1, hero_y)
+
+    def change_coordinates(self, x, y, direction):
+        if self.check_valid_move(direction):
+            if direction == "right":
+                return (x, y + 1)
+            elif direction == "left":
+                return (x, y - 1)
+            elif direction == "up":
+                return (x - 1, y)
+            elif direction == "down":
+                return (x + 1, y)
+
+    def swap_elements(self, x, y, direction, element):
+        new_x, new_y = self.change_coordinates(x, y, direction)
+        if element == '.':
+            self.map[x][y], self.map[new_x][new_y] = self.map[new_x][new_y], self.map[x][y]
+        elif element == 'T':
+            self.map[x][y], self.map[new_x][new_y] = ".", self.map[x][y]
+
     def move(self, direction):
         assert type(direction) is str
+        if not self.check_valid_move(direction):
+            return False
+        else:
+            hero_x, hero_y = self.hero_coordinates()
+            new_x, new_y = self.change_coordinates(hero_x, hero_y, direction)
+            if self.get_element_from_coordinates(new_x, new_y) == "#":
+                return False
+            elif self.get_element_from_coordinates(new_x, new_y) == "E":
+                pass
+                # TODO: fight
+            elif self.get_element_from_coordinates(new_x, new_y) == "T":
+                print("Found treasure!")
+                # self.swap_elements(hero_x, hero_y, direction, ".")
+                # TODO: random treasures
+            elif self.get_element_from_coordinates(new_x, new_y) == ".":
+                self.swap_elements(hero_x, hero_y, direction, ".")
+                return True
+
 
--- dungeon_tests.py
+++ dungeon_tests.py
@@ -27,6 +27,65 @@ class DungeonTests(unittest.TestCase):
         with self.assertRaises(AssertionError):
             self.d.move(5)
 
+    # def test_print_map(self):
+    #     self.d.print_map()
+
+    def test_is_valid_coordinate_true(self):
+        self.assertTrue(self.d.is_valid_coordinate(4, 6))
+
+    def test_is_valid_coordinate_false(self):
+        self.assertFalse(self.d.is_valid_coordinate(8, 10))
+
+    def test_get_coordinate_true(self):
+        expected = (0, 9)
+        self.assertEqual(self.d.get_coordinate('T'), expected)
+
+    def test_get_coordinate_false(self):
+        self.assertFalse(self.d.get_coordinate('P'))
+
+    def test_hero_coordinates_false(self):
+        self.assertFalse(self.d.hero_coordinates())
+
+    def test_hero_coordinates(self):
+        self.d.spawn(self.test_hero)
+        expected = (0, 0)
+        self.assertEqual(self.d.hero_coordinates(), expected)
+
+    def test_check_valid_move_left(self):
+        self.d.spawn(self.test_hero)
+        self.assertFalse(self.d.check_valid_move("left"))
+
+    def test_check_valid_move_right(self):
+        self.d.spawn(self.test_hero)
+        self.assertTrue(self.d.check_valid_move("right"))
+
+    def test_check_valid_move_up(self):
+        self.d.spawn(self.test_hero)
+        self.assertFalse(self.d.check_valid_move("up"))
+
+    def test_check_valid_move_down(self):
+        self.d.spawn(self.test_hero)
+        self.assertTrue(self.d.check_valid_move("down"))
+
+    def test_get_element_from_coordinates_invalid_coordinates(self):
+        self.assertEqual(self.d.get_element_from_coordinates(5, 10), None)
+
+    def test_get_element_from_coodinates_valid_coordinates(self):
+        self.assertEqual(self.d.get_element_from_coordinates(2, 5), 'E')
+
+    def test_move_false(self):
+        self.d.spawn(self.test_hero)
+        self.assertFalse(self.d.move("up"))
+
+    def test_move_obstacle(self):
+        self.d.spawn(self.test_hero)
+        self.assertFalse(self.d.move("down"))
+
+    def test_move_path(self):
+        self.d.spawn(self.test_hero)
+        self.assertTrue(self.d.move("right"))
+        self.d.print_map()
+
 
 if __name__ == "__main__":
     unittest.main()
