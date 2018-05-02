971b3f8b1f0fb8b0409311ce41e507140024efd5
Daniel Goshev <danielstoynev@yahoo.com>
2018-05-02 19:28:40 +0300

    Dungeons and tests for Dungeons

--- dungeons_and_pythons.py
+++ dungeons_and_pythons.py
@@ -36,6 +36,8 @@ class Dungeon:
     def spawn(self, hero):
         for i in range(len(self.dungeon)):
             try:
+                if hero.__class__ != Hero:
+                    raise AssertionError
                 line_index = self.dungeon[i].index('S')
                 self.dungeon[i][line_index] = 'H'
                 self.hero_position = (i, line_index)
@@ -43,6 +45,9 @@ class Dungeon:
                 if (i, line_index) in self.save_respawn_points:
                     self.save_respawn_points.remove((i, line_index))
                 return True
+            except AssertionError:
+                print("Wrong hero")
+                return False
             except ValueError:
                 print('Game over')
                 return False
@@ -194,31 +199,31 @@ It deals {} damage for the cost of {} mana in range of {}!'.format(found[1],
                 print("You found a bag of potatoes!")
 
 
-def main():
-    weapon = Weapon(name="The Axe of Destiny", damage=20)
-    spell = Spell(name="Fireball", damage=30,
-                  mana_cost=50, cast_range=2)
-    one = Dungeon("dungeon1.txt")
-    one.print_map()
-    my_hero = Hero(
-        name="Bron", title="Dragonslayer",
-        health=100, mana=100, mana_regeneration_rate=2)
-    my_hero.equip(weapon)
-    my_hero.learn(spell)
-    my_enemy = Enemy(health=100, mana=100, damage=20)
-    one.spawn(my_hero)
-    one.set_enemy(my_enemy)
-    one.print_map()
-    print(one.move_hero("right"))
-    one.print_map()
-    print(one.move_hero("down"))
-    one.print_map()
-    one.hero_attack(by='spell')
-    print(one.move_hero("down"))
-    one.print_map()
-    one.hero_attack(by='spell')
-    one.print_map()
+# def main():
+#     weapon = Weapon(name="The Axe of Destiny", damage=20)
+#     spell = Spell(name="Fireball", damage=30,
+#                   mana_cost=50, cast_range=2)
+#     one = Dungeon("dungeon1.txt")
+#     one.print_map()
+#     my_hero = Hero(
+#         name="Bron", title="Dragonslayer",
+#         health=100, mana=100, mana_regeneration_rate=2)
+#     my_hero.equip(weapon)
+#     my_hero.learn(spell)
+#     my_enemy = Enemy(health=100, mana=100, damage=20)
+#     one.spawn(my_hero)
+#     one.set_enemy(my_enemy)
+#     one.print_map()
+#     print(one.move_hero("right"))
+#     one.print_map()
+#     print(one.move_hero("down"))
+#     one.print_map()
+#     one.hero_attack(by='spell')
+#     print(one.move_hero("down"))
+#     one.print_map()
+#     one.hero_attack(by='spell')
+#     one.print_map()
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
